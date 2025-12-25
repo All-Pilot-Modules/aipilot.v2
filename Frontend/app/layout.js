5d13e6f747a837597583fc85b43a970dbd3cee36
Yubraj977 <yubrajkhatri977@gmail.com>
2025-12-25 00:09:00 -0500

    analytics

--- Frontend/app/layout.js
+++ Frontend/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/next";
 import { ClientProviders } from "@/components/ClientProviders";
 
 
